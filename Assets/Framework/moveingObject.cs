6825ded3cecd2cfd58a63c6c1977c728fa08d2a2
Alex Casas <buddy123.at@gmail.com>
2021-11-23 13:04:32 -0600

    Added buttons and doors button needs work

---
+++ Assets/Framework/moveingObject.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveingObject : MonoBehaviour, Toggleable
+{
+    [SerializeField] Transform objectToMove;
+    [SerializeField] float transitionTime;
+    Coroutine MoveingCoroutine;
+
+    public Transform StartTrans;
+    public Transform EndTrans;
+
+    public void ToggleOn()
+    {
+        MoveTo(true);
+    }
+
+    public void ToggleOff()
+    {
+        MoveTo(false);
+    }
+
+    public void MoveTo(bool ToEnd)
+    {
+        if (ToEnd)
+        {
+            MoveTo(EndTrans);
+        }
+        else
+        {
+            MoveTo(StartTrans);
+        }
+    }
+
+    public void MoveTo(Transform Destination)
+    {
+        if (MoveingCoroutine != null)
+        {
+            StopCoroutine(MoveingCoroutine);
+            MoveingCoroutine = null;
+        }
+        MoveingCoroutine = StartCoroutine(MoveToTrans(Destination, transitionTime));
+    }
+
+    IEnumerator MoveToTrans(Transform Destination, float TransitionTime)
+    {
+        Vector3 StartPos = transform.position;
+        Vector3 EndPos = Destination.position;
+        Quaternion StartRot = transform.rotation;
+        Quaternion EndRot = Destination.rotation;
+
+        float timmer = 0f;
+        while (timmer < TransitionTime)
+        {
+            timmer += Time.deltaTime;
+            objectToMove.position = Vector3.Lerp(StartPos, EndPos, timmer / TransitionTime);
+            objectToMove.rotation = Quaternion.Lerp(StartRot, EndRot, timmer / TransitionTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/Prefabs/Interactables/Buttons/ButtonScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    [SerializeField] GameObject ToggelingObject;
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Box")
+        {
+            Debug.Log("Button unpressed");
+            OnObjectLeft();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.name == "Stadard_Box")
+        {
+            Debug.Log("Button Pressed");
+            OnObjectPlaced();
+        }
+    }
+
+    public void OnObjectLeft()
+    {
+        gameObject.GetComponent<Toggleable>().ToggleOff();
+        ToggelingObject.GetComponent<Toggleable>().ToggleOff();
+    }
+
+    public void OnObjectPlaced()
+    {
+        gameObject.GetComponent<Toggleable>().ToggleOn();
+        ToggelingObject.GetComponent<Toggleable>().ToggleOn();
+    }
+}
